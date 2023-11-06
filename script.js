9263d3e8aec7f3d486063a4fa3f4cdb8a439e41d
suhanibansal870 <83600598+suhanibansal870@users.noreply.github.com>
2023-11-05 18:50:49 -0800

    Initial upload draft

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    var popup_btn = $('.popup-btn');
+    popup_btn.magnificPopup({
+    type : 'image',
+    gallery : {
+        enabled : true
+    }
+    });
+    });
